dec96fb703007f85dbe9900de31b4e2724de5c3a
Jailson Pereira <39743726+JPereirax@users.noreply.github.com>
2018-10-29 23:34:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const request = require('request-promise');
 
+let version = '8.21.1';
+let language = 'en_US';
+
 let options = {
     simple: false,
     resolveWithFullResponse: true,
@@ -26,12 +29,65 @@ module.exports = function RiotAPI(api_key) {
             return "API Key not found.";
 
         return new Promise((resolve, reject) => {
-            request('https://' + region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + name + '?api_key=' + api_key, options)
+            let uri = 'https://' + region + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + name + '?api_key=' + api_key;
+            request(uri, options)
                 .then(data => {
                     resolve(data.body);
                 }).catch(err => {
                     return reject(err);
                 });
+        });
+    };
+
+    this.getIconBySummonerId = async (region, name) => {
+        if (api_key == null)
+            return "API Key not found.";
+
+        return new Promise(async (resolve) => {
+            let summoner = await this.getSummonerByName(region, name);
+            resolve('http://ddragon.leagueoflegends.com/cdn/' + version + '/img/profileicon/' + summoner.profileIconId + '.png');
+        });
+    };
+
+    this.getChampions = async () => {
+        if (api_key == null)
+            return "API Key not found.";
+
+        return new Promise((resolve, reject) => {
+            let uri = 'http://ddragon.leagueoflegends.com/cdn/' + version + '/data/' + language + '/champion.json';
+            request(uri, options).then(data => {
+                resolve(data.body.data);
+            }).catch(err => {
+                return reject(err);
+            });
+        });
+    };
+
+    this.getChampionsById = async (championId) => {
+        if (api_key == null)
+            return "API Key not found.";
+
+        return new Promise(async (resolve) => {
+            let data = await this.getChampions();
+            for (champion in data) {
+                if (data[champion].key == championId) {
+                    resolve(data[champion]);
+                }
+            }
+        });
+    }
+
+    this.getFreeToPlayChampions = async (region) => {
+        if (api_key == null)
+            return "API Key not found.";
+
+        return new Promise(async (resolve, reject) => {
+            let uri = 'https://' + region + '.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=' + api_key;
+            request(uri, options).then(data => {
+                resolve(data.body.freeChampionIds);
+            }).catch(err => {
+                return reject(err);
+            });
         });
     };
 
